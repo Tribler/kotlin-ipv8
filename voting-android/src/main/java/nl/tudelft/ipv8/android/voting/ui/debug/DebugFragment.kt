9772b98068b5ccc1b20010a6182bdde839597a50
EmieldeSmidt <desmidt.emiel@gmail.com>
2020-03-03 12:28:58 +0100

    Added decoupled voting application

---
+++ voting-android/src/main/java/nl/tudelft/ipv8/android/voting/ui/debug/DebugFragment.kt
@@ -0,0 +1,61 @@
+package nl.tudelft.ipv8.android.voting.ui.debug
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import kotlinx.android.synthetic.main.fragment_debug.*
+import kotlinx.coroutines.*
+import nl.tudelft.ipv8.Community
+import nl.tudelft.ipv8.android.voting.R
+import nl.tudelft.ipv8.android.voting.ui.BaseFragment
+import nl.tudelft.ipv8.util.toHex
+
+class DebugFragment : BaseFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_debug, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        lifecycleScope.launchWhenStarted {
+            while (isActive) {
+                updateView()
+                delay(1000)
+            }
+        }
+    }
+
+    private fun updateView() {
+        val ipv8 = getIpv8()
+        val demo = getDemoCommunity()
+        txtBootstrap.text = Community.DEFAULT_ADDRESSES.joinToString("\n")
+        txtLanAddress.text = demo.myEstimatedLan.toString()
+        txtWanAddress.text = demo.myEstimatedWan.toString()
+        txtPeerId.text = ipv8.myPeer.mid
+        txtPublicKey.text = ipv8.myPeer.publicKey.keyToBin().toHex()
+        txtOverlays.text = ipv8.overlays.values.toList().joinToString("\n") {
+            it.javaClass.simpleName + " (" + it.getPeers().size + " peers)"
+        }
+
+        lifecycleScope.launch {
+            val blockCount = withContext(Dispatchers.IO) {
+                getTrustChainCommunity().database.getAllBlocks().size
+            }
+            txtBlockCount.text = blockCount.toString()
+        }
+
+        lifecycleScope.launch {
+            val chainLength = withContext(Dispatchers.IO) {
+                getTrustChainCommunity().getChainLength()
+            }
+            txtChainLength.text = chainLength.toString()
+        }
+    }
+}
