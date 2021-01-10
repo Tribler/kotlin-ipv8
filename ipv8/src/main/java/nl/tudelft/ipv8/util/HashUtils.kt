459f3dec31e7115ae1769f72e048142d79594210
Rowdy Chotkan <rowdy_chotkan@msn.com>
2021-01-10 18:06:28 +0100

    Add sha3-256

--- ipv8/src/main/java/nl/tudelft/ipv8/util/HashUtils.kt
+++ ipv8/src/main/java/nl/tudelft/ipv8/util/HashUtils.kt
@@ -4,6 +4,7 @@ import java.security.MessageDigest
 
 private const val SHA1 = "SHA-1"
 private const val SHA256 = "SHA-256"
+private const val SHA3_256 = "SHA3-256"
 
 fun sha1(input: ByteArray): ByteArray {
     return MessageDigest
@@ -15,4 +16,8 @@ fun sha256(input: ByteArray): ByteArray {
     return MessageDigest
         .getInstance(SHA256)
         .digest(input)
+}
+
+fun sha3_256(input: ByteArray): ByteArray {
+    return MessageDigest.getInstance(SHA3_256).digest(input)
 }
