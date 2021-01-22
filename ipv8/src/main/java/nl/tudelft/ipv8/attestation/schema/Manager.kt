6210bdeebfef43c2a15fbf135505ba6ed2877a47
Rowdy Chotkan <rowdy_chotkan@msn.com>
2021-01-22 15:26:02 +0100

    Add schemamanager

---
+++ ipv8/src/main/java/nl/tudelft/ipv8/attestation/schema/Manager.kt
@@ -0,0 +1,91 @@
+package nl.tudelft.ipv8.attestation.schema
+
+import nl.tudelft.ipv8.attestation.attestation.IdentityAlgorithm
+import nl.tudelft.ipv8.attestation.attestation.WalletAttestation
+import nl.tudelft.ipv8.attestation.wallet.bonehexact.attestations.BitPairAttestation
+import nl.tudelft.ipv8.keyvault.JavaCryptoProvider
+import nl.tudelft.ipv8.keyvault.PrivateKey
+import nl.tudelft.ipv8.keyvault.PublicKey
+import nl.tudelft.ipv8.keyvault.defaultCryptoProvider
+
+class SchemaManager {
+
+    private val formats = HashMap<String, HashMap<String, Any>>()
+    private val algorithms = HashMap<String, Class<IdentityAlgorithm>>()
+
+
+    fun getAlgorithmClass(algorithmName: String): Class<IdentityAlgorithm> {
+        if (algorithmName in this.algorithms.keys) {
+            return this.algorithms[algorithmName]!!
+        }
+
+        lateinit var algorithm: IdentityAlgorithm
+        when (algorithmName) {
+            "bonehexact" -> {
+                TODO()
+                algorithm = TODO()
+            }
+            "pengbaorange" -> {
+                TODO()
+                algorithm = TODO()
+            }
+            "irmaexact" -> {
+                TODO()
+                algorithm = TODO()
+            }
+            else -> {
+                throw RuntimeException("Attempted to load unknown proof algorithm: ${algorithmName}.")
+            }
+        }
+
+        this.algorithms[algorithmName] = algorithm
+        return algorithm
+
+    }
+
+    fun registerSchema(
+        schemaName: String,
+        algorithmName: String,
+        parameters: HashMap<String, Any>,
+    ) {
+        val schema = HashMap<String, Any>()
+        schema["algorithm"] = algorithmName
+        schema.putAll(parameters)
+        this.formats[schemaName] = schema
+    }
+
+    fun registerDefaultSchemas() {
+        TODO()
+    }
+
+    fun getAlgorithmInstance(schemaName: String): IdentityAlgorithm {
+        val schema = this.formats[schemaName]
+        val identityAlgorithmClass = this.getAlgorithmClass(schema!!["algorithm"] as String)
+        return identityAlgorithmClass.getDeclaredConstructor()
+            .newInstance(schemaName, this.formats)
+    }
+
+    fun getAlgorithmName(idFormat: String): String {
+        return this.formats[idFormat]?.get("algorithm").toString()
+    }
+
+    fun deserializePrivate(idFormat: String, privateKey: PrivateKey, attestationBlob: ByteArray): WalletAttestation {
+        when (val algorithmName = this.getAlgorithmName(idFormat)) {
+            "bonehexact" -> {
+                return TODO()
+            }
+            "pengbaorange" -> {
+                return TODO()
+
+            }
+            "irmaexact" -> {
+                TODO()
+                return TODO()
+            }
+            else -> {
+                throw RuntimeException("Attempted to load unknown proof algorithm: ${algorithmName}.")
+            }
+        }
+    }
+
+}
