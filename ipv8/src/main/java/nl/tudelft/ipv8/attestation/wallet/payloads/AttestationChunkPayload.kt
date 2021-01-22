2832e63f77d577eb697e5afc1dac37fd539247d7
Rowdy Chotkan <rowdy_chotkan@msn.com>
2021-01-22 15:21:13 +0100

    Add AttestationChunkPayload

---
+++ ipv8/src/main/java/nl/tudelft/ipv8/attestation/wallet/payloads/AttestationChunkPayload.kt
@@ -0,0 +1,38 @@
+package nl.tudelft.ipv8.attestation.wallet.payloads
+
+import nl.tudelft.ipv8.messaging.*
+
+class AttestationChunkPayload(
+    val hash: ByteArray,
+    val sequenceNumber: Int,
+    val data: ByteArray
+) : Serializable {
+    private val msgId = 2
+
+    override fun serialize(): ByteArray {
+        return hash + serializeUInt(sequenceNumber as UInt) + serializeVarLen(data)
+    }
+
+    companion object Deserializer : Deserializable<AttestationChunkPayload> {
+        override fun deserialize(
+            buffer: ByteArray,
+            offset: Int
+        ): Pair<AttestationChunkPayload, Int> {
+            var localoffset = 0
+
+            val hash = buffer.copyOfRange(offset + localoffset,
+                offset + localoffset + SERIALIZED_SHA1_HASH_SIZE)
+            localoffset += SERIALIZED_SHA1_HASH_SIZE
+
+            val sequenceNumber = deserializeUInt(buffer, offset + localoffset)
+            localoffset += SERIALIZED_UINT_SIZE
+
+            val (data, dataSize) = deserializeVarLen(buffer, offset + localoffset)
+            localoffset += dataSize
+
+            val payload = AttestationChunkPayload(hash, sequenceNumber.toInt(), data)
+            return Pair(payload, localoffset)
+        }
+
+    }
+}
