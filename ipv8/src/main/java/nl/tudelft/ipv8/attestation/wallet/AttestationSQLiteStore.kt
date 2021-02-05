a3ee9807fc95814d30821d1a46f33580f901694b
Rowdy Chotkan <rowdy_chotkan@msn.com>
2021-02-05 17:27:33 +0100

    Implement database methods

--- ipv8/src/main/java/nl/tudelft/ipv8/attestation/wallet/AttestationSQLiteStore.kt
+++ ipv8/src/main/java/nl/tudelft/ipv8/attestation/wallet/AttestationSQLiteStore.kt
@@ -1,26 +1,40 @@
 package nl.tudelft.ipv8.attestation.wallet
 
 import nl.tudelft.ipv8.attestation.WalletAttestation
-import nl.tudelft.ipv8.keyvault.PrivateKey
+import nl.tudelft.ipv8.attestation.wallet.cryptography.bonehexact.BonehPrivateKey
 import nl.tudelft.ipv8.sqldelight.Database
 
-class AttestationSQLiteStore(database: Database) : AttestationStore {
-    private val dao = database.dbBlockQueries
 
-    override fun getAttestationByHash(attestationHash: ByteArray): ByteArray {
-        TODO()
+private val attestationMapper: (
+    ByteArray,
+    ByteArray,
+    ByteArray,
+    String,
+) -> AttestationBlob = { hash, blob, key, id_format ->
+    AttestationBlob(
+        hash, blob, key, id_format
+    )
+}
+
+
+class AttestationSQLiteStore(database: Database) : AttestationStore {
+    private val dao = database.dbAttestationQueries
+
+    override fun getAttestationByHash(attestationHash: ByteArray): ByteArray? {
+        return dao.getAttestationByHash(attestationHash).executeAsOneOrNull()
     }
 
-    override fun getAll(): List<ByteArray> {
-        TODO("Not yet implemented")
+    override fun getAll(): List<AttestationBlob> {
+        return dao.getAll(attestationMapper).executeAsList()
     }
 
     override fun insertAttestation(
         attestation: WalletAttestation,
         attestationHash: ByteArray,
-        secretKey: PrivateKey,
+        secretKey: BonehPrivateKey,
         idFormat: String,
     ) {
-        TODO("Not yet implemented")
+        val blob = attestation.serializePrivate(secretKey.publicKey())
+        dao.insertAttestation(attestationHash, blob, secretKey.serialize(), idFormat)
     }
 }
