b38933c6fa052e243d9eb3eb370b752438259d27
Rowdy Chotkan <rowdy_chotkan@msn.com>
2021-03-29 10:41:28 +0200

    Update AuthorityManager

---
+++ ipv8/src/main/java/nl/tudelft/ipv8/attestation/revocation/AuthoritySQLiteStore.kt
@@ -0,0 +1,121 @@
+package nl.tudelft.ipv8.attestation.revocation
+
+import nl.tudelft.ipv8.attestation.Authority
+import nl.tudelft.ipv8.keyvault.PublicKey
+import nl.tudelft.ipv8.keyvault.defaultCryptoProvider
+import nl.tudelft.ipv8.sqldelight.Database
+import nl.tudelft.ipv8.sqldelight.GetAllRevocations
+
+private val authorityMapper: (
+    ByteArray?,
+    ByteArray,
+    Long?,
+    Long?,
+) -> Authority = { public_key, hash, version, recognized ->
+    Authority(public_key?.let { defaultCryptoProvider.keyFromPublicBin(it) },
+        hash,
+        version ?: 0L,
+        recognized?.toInt() == 1)
+}
+
+class AuthoritySQLiteStore(database: Database) : AuthorityStore {
+    private val dao = database.dbAuthorityQueries
+
+    override fun getKnownAuthorities(): List<Authority> {
+        return dao.getAllAuthorities(authorityMapper).executeAsList()
+    }
+
+    override fun getRecognizedAuthorities(): List<Authority> {
+        return dao.getAllRecognizedAuthorities(authorityMapper).executeAsList()
+    }
+
+    override fun getAuthorityByHash(hash: ByteArray): Authority? {
+        return dao.getAuthorityByHash(hash, authorityMapper).executeAsOneOrNull()
+    }
+
+    override fun recognizeAuthority(hash: ByteArray) {
+        return dao.recognizeAuthority(hash)
+    }
+
+    override fun disregardAuthority(hash: ByteArray) {
+        return dao.disregardAuthority(hash)
+    }
+
+    override fun insertAuthority(publicKey: PublicKey) {
+        return dao.insertAuthority(publicKey.keyToBin(), publicKey.keyToHash(), null, null)
+    }
+
+    override fun insertAuthority(hash: ByteArray) {
+        return dao.insertAuthority(null, hash, null, null)
+    }
+
+    override fun insertRevocations(
+        publicKeyHash: ByteArray,
+        version: Long,
+        signature: ByteArray,
+        revokedHashes: List<ByteArray>,
+    ) {
+        var authorityId = dao.getAuthorityIdByHash(publicKeyHash).executeAsOneOrNull()
+        if (authorityId == null) {
+            this.insertAuthority(publicKeyHash)
+            authorityId = dao.getAuthorityIdByHash(publicKeyHash).executeAsOne()
+        }
+
+        var versionId =
+            dao.getVersionByAuthorityIDandVersionNumber(authorityId, version).executeAsOneOrNull()?.version_id
+        if (versionId == null) {
+            dao.insertVersion(authorityId, version, signature)
+            versionId = dao.getVersionByAuthorityIDandVersionNumber(authorityId, version).executeAsOne().version_id
+        }
+
+        revokedHashes.forEach { dao.insertRevocation(authorityId, versionId, it) }
+        dao.updateVersionFor(versionId, publicKeyHash)
+    }
+
+    override fun getVersionsSince(publicKeyHash: ByteArray, sinceVersion: Long): List<Long> {
+        val authorityId = dao.getAuthorityIdByHash(publicKeyHash).executeAsOneOrNull()
+        return if (authorityId == null) {
+            emptyList()
+        } else
+            dao.getVersionsSince(authorityId, sinceVersion).executeAsList()
+    }
+
+    override fun getRevocations(
+        publicKeyHash: ByteArray,
+        versions: List<Long>,
+    ): List<RevocationBlob> {
+        val authorityId = dao.getAuthorityIdByHash(publicKeyHash).executeAsOne()
+        val versionEntries =
+            dao.getVersionsByAuthorityIDandVersionNumbers(authorityId, versions).executeAsList()
+
+        return versionEntries.map {
+            RevocationBlob(it.version_number,
+                it.signature,
+                dao.getRevocationsByAuthorityIdAndVersionId(authorityId, it.version_id)
+                    .executeAsList())
+        }
+    }
+
+    override fun getAllRevocations(): List<GetAllRevocations> {
+        return dao.getAllRevocations().executeAsList()
+    }
+
+
+//    fun insertAuthority(publicKey: PublicKey, hash: String, version: Long, recognized: Boolean) {
+//        val keyBin = publicKey.keyToBin()
+//        dao.insertAuthority(keyBin, hash, version, if (recognized) 1 else 0)
+//    }
+//
+//    fun getAuthorityByPublicKey(publicKey: PublicKey): Authority? {
+//        return dao.getAuthorityByPublicKey(publicKey.keyToBin(), authorityMapper).executeAsOneOrNull()
+//    }
+//
+//    fun getAuthorityByHash(hash: String): Authority? {
+//        return dao.getAuthorityByHash(hash, authorityMapper).executeAsOneOrNull()
+//    }
+//
+//    fun deleteAuthorityByHash(hash: String) {
+//        return dao.deleteAuthorityByHash(hash)
+//    }
+
+}
---
+++ ipv8/src/main/java/nl/tudelft/ipv8/attestation/revocation/AuthorityStore.kt
@@ -0,0 +1,33 @@
+package nl.tudelft.ipv8.attestation.revocation
+
+import nl.tudelft.ipv8.attestation.Authority
+import nl.tudelft.ipv8.attestation.Revocations
+import nl.tudelft.ipv8.keyvault.PublicKey
+import nl.tudelft.ipv8.sqldelight.GetAllRevocations
+
+class RevocationBlob(val version: Long, val signature: ByteArray, val revocations: List<ByteArray>)
+
+interface AuthorityStore {
+
+    fun getKnownAuthorities(): List<Authority>
+    fun getRecognizedAuthorities(): List<Authority>
+    fun getAuthorityByHash(hash: ByteArray): Authority?
+    fun recognizeAuthority(hash: ByteArray)
+    fun disregardAuthority(hash: ByteArray)
+    fun insertAuthority(publicKey: PublicKey)
+    fun insertAuthority(hash: ByteArray)
+
+    fun insertRevocations(
+        publicKeyHash: ByteArray,
+        version: Long,
+        signature: ByteArray,
+        revokedHashes: List<ByteArray>,
+    )
+
+    fun getRevocations(publicKeyHash: ByteArray, versions: List<Long>): List<RevocationBlob>
+
+    fun getVersionsSince(publicKeyHash: ByteArray, sinceVersion: Long): List<Long>
+
+    fun getAllRevocations(): List<GetAllRevocations>
+
+}
