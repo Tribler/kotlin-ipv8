210b14d9a119c31643f605051b62f52bf38bd523
Rowdy Chotkan <rowdy_chotkan@msn.com>
2021-03-31 11:22:10 +0200

    Rename parameter in PeerCache

--- ipv8/src/main/java/nl/tudelft/ipv8/attestation/wallet/caches/PeerCache.kt
+++ ipv8/src/main/java/nl/tudelft/ipv8/attestation/wallet/caches/PeerCache.kt
@@ -3,8 +3,8 @@ package nl.tudelft.ipv8.attestation.wallet.caches
 import nl.tudelft.ipv8.attestation.wallet.RequestCache
 import java.math.BigInteger
 
-open class PeerCache(val cacheHash: RequestCache, prefix: String, val mid: String, val idFormat: String) :
-    NumberCache(cacheHash, prefix, idFromAddress(prefix, mid).second) {
+open class PeerCache(val cache: RequestCache, prefix: String, val mid: String, val idFormat: String) :
+    NumberCache(cache, prefix, idFromAddress(prefix, mid).second) {
 
     companion object {
         fun idFromAddress(prefix: String, mid: String): Pair<String, BigInteger> {
