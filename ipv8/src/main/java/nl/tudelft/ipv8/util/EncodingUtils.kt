d2a410626044839669c6ecb18c450948936ae80f
Rowdy Chotkan <rowdy_chotkan@msn.com>
2021-04-25 21:21:49 +0200

    Add toMap function for JSONObject

--- ipv8/src/main/java/nl/tudelft/ipv8/util/EncodingUtils.kt
+++ ipv8/src/main/java/nl/tudelft/ipv8/util/EncodingUtils.kt
@@ -1,5 +1,8 @@
 package nl.tudelft.ipv8.util
 
+import org.json.JSONArray
+import org.json.JSONObject
+
 interface EncodingUtils {
 
     fun encodeBase64(bin: ByteArray): ByteArray
@@ -12,3 +15,21 @@ interface EncodingUtils {
 }
 
 var defaultEncodingUtils: EncodingUtils = JavaEncodingUtils
+
+fun JSONObject.asMap(): Map<String, Any?> {
+    val results: MutableMap<String, Any?> = HashMap()
+    for (key in this.keys()) {
+        val value1 = this[key]
+        val value: Any? = if (value1 == null || JSONObject.NULL == value1) {
+            null
+        } else if (value1 is JSONObject) {
+            value1.asMap()
+        } else if (value1 is JSONArray) {
+            value1.toList()
+        } else {
+            value1
+        }
+        results[key] = value
+    }
+    return results
+}
