3fea5fbc630b467549c68bcae2e18ef8bdfbc9ec
Rowdy Chotkan <rowdy_chotkan@msn.com>
2021-04-29 09:45:17 +0200

    Create JSONArray toList method

--- ipv8/src/main/java/nl/tudelft/ipv8/util/EncodingUtils.kt
+++ ipv8/src/main/java/nl/tudelft/ipv8/util/EncodingUtils.kt
@@ -2,6 +2,7 @@ package nl.tudelft.ipv8.util
 
 import org.json.JSONArray
 import org.json.JSONObject
+import java.util.ArrayList
 
 interface EncodingUtils {
 
@@ -25,11 +26,28 @@ fun JSONObject.asMap(): Map<String, Any?> {
         } else if (value1 is JSONObject) {
             value1.asMap()
         } else if (value1 is JSONArray) {
-            value1.toList()
+            value1.asList()
         } else {
             value1
         }
         results[key] = value
+    }
+    return results
+}
+
+fun JSONArray.asList(): List<Any?> {
+    val results: MutableList<Any?> = ArrayList(this.length())
+    for (i in 0 until this.length()) {
+        val element = this.get(i)
+        if (element == null || JSONObject.NULL == element) {
+            results.add(null)
+        } else if (element is JSONArray) {
+            results.add(element.asList())
+        } else if (element is JSONObject) {
+            results.add(element.asMap())
+        } else {
+            results.add(element)
+        }
     }
     return results
 }
