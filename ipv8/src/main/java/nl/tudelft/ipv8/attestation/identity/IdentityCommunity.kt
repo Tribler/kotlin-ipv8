79efec495b15bda3317d4a60453e6c0287ea71c5
Rowdy Chotkan <rowdy_chotkan@msn.com>
2021-04-29 09:43:57 +0200

    Add debug log + change default value types

--- ipv8/src/main/java/nl/tudelft/ipv8/attestation/identity/IdentityCommunity.kt
+++ ipv8/src/main/java/nl/tudelft/ipv8/attestation/identity/IdentityCommunity.kt
@@ -142,6 +142,7 @@ class IdentityCommunity(
         val transaction = JSONObject(String(metadata.serializedMetadata))
         val requestedKeys = transaction.keySet()
         if (!pseudonym.tree.elements.containsKey(metadata.tokenPointer.toKey())) {
+            logger.debug("Not signing $metadata, unknown token!")
             return false
         }
         val attributeHash = pseudonym.tree.elements[metadata.tokenPointer.toKey()]!!.contentHash
