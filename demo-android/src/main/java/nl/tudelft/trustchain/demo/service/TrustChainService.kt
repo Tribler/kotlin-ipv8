245f68e413f6d16ccac2eb59b653ef8809e0ec43
Rowdy Mitchell Chotkan <rowdy_chotkan@msn.com>
2024-02-19 15:07:17 +0100

    Upgrade `demo-android` + migrate to view binding

--- demo-android/src/main/java/nl/tudelft/trustchain/demo/service/TrustChainService.kt
+++ demo-android/src/main/java/nl/tudelft/trustchain/demo/service/TrustChainService.kt
@@ -11,14 +11,11 @@ import nl.tudelft.trustchain.demo.ui.DemoActivity
 class TrustChainService : IPv8Service() {
     override fun createNotification(): NotificationCompat.Builder {
         val trustChainExplorerIntent = Intent(this, DemoActivity::class.java)
-        val pendingIntent = TaskStackBuilder.create(this)
-            .addNextIntentWithParentStack(trustChainExplorerIntent)
+        val pendingIntent = TaskStackBuilder.create(this).addNextIntentWithParentStack(trustChainExplorerIntent)
             .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
 
-        return NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_CONNECTION)
-            .setContentTitle("IPv8 Demo")
-            .setContentText("Running")
-            .setSmallIcon(R.drawable.ic_insert_link_black_24dp)
+        return NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_CONNECTION).setContentTitle("IPv8 Demo")
+            .setContentText("Running").setSmallIcon(R.drawable.ic_insert_link_black_24dp)
             .setContentIntent(pendingIntent)
     }
 }
