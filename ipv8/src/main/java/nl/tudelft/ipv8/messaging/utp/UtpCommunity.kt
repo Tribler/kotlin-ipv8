a6adba07cbdaf101bdbb7e10eab7b07fa490da91
Eragoneq <eragon.skill@proton.me>
2024-04-08 16:32:55 +0200

    Add custom UTP community

---
+++ ipv8/src/main/java/nl/tudelft/ipv8/messaging/utp/UtpCommunity.kt
@@ -0,0 +1,11 @@
+package nl.tudelft.ipv8.messaging.utp
+
+import nl.tudelft.ipv8.Community
+
+class UtpCommunity : Community() {
+    override val serviceId = "450ded7389134595dadb6b2549f431ad60156931"
+
+
+
+
+}
