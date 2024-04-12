8f766de2a999f9fd0fc84d1624a30bbf5696307e
Eragoneq <eragon.skill@proton.me>
2024-04-12 04:07:48 +0200

    Fix infinite loop cancellation issue
    Signed-off-by: Eragoneq <eragon.skill@proton.me>

--- ipv8/src/main/java/nl/tudelft/ipv8/messaging/utp/UtpHelper.kt
+++ ipv8/src/main/java/nl/tudelft/ipv8/messaging/utp/UtpHelper.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withTimeoutOrNull
+import kotlinx.coroutines.yield
 import nl.tudelft.ipv8.IPv4Address
 import nl.tudelft.ipv8.Peer
 import nl.tudelft.ipv8.messaging.payload.TransferRequestPayload
@@ -28,11 +29,12 @@ class UtpHelper(
                 metadata.size,
                 TransferRequestPayload.TransferType.FILE
             )
-
             // Wait for response or timeout
+            // TODO: This should be handled with a proper pattern instead of a timeout
             println("Waiting for response from $peer")
             val payload = withTimeoutOrNull(5000) {
-                while (!utpCommunity.transferRequests.containsKey(peer.mid)) { /* no-op */
+                while (!utpCommunity.transferRequests.containsKey(peer.mid)) {
+                    yield()
                 }
                 println("Received response from $peer")
                 return@withTimeoutOrNull utpCommunity.transferRequests.remove(peer.mid)
