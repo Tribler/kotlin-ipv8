6d19d5b69f084b8caed09de74a18a700397e3e68
Rowdy Mitchell Chotkan <rowdy_chotkan@msn.com>
2025-02-07 16:12:39 +0100

    Remove unnecessary `isActive`

--- demo-android/src/main/java/nl/tudelft/trustchain/demo/ui/DemoActivity.kt
+++ demo-android/src/main/java/nl/tudelft/trustchain/demo/ui/DemoActivity.kt
@@ -12,7 +12,6 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.mattskala.itemadapter.ItemAdapter
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import nl.tudelft.ipv8.android.IPv8Android
 import nl.tudelft.trustchain.demo.DemoCommunity
@@ -44,53 +43,54 @@ class DemoActivity : AppCompatActivity() {
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
         binding.recyclerView.addItemDecoration(DividerItemDecoration(this, LinearLayout.VERTICAL))
 
-        loadNetworkInfo()
-    }
-
-    private fun loadNetworkInfo() {
         lifecycleScope.launch {
             lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                while (isActive) {
-                    val demoCommunity = IPv8Android.getInstance().getOverlay<DemoCommunity>()!!
-                    val peers = demoCommunity.getPeers()
-
-                    val discoveredAddresses = demoCommunity.network.getWalkableAddresses(demoCommunity.serviceId)
-
-                    val discoveredBluetoothAddresses =
-                        demoCommunity.network.getNewBluetoothPeerCandidates().map { it.address }
-
-                    val peerItems = peers.map {
-                        PeerItem(
-                            it
-                        )
-                    }
-
-                    val addressItems = discoveredAddresses.map { address ->
-                        val contacted = demoCommunity.discoveredAddressesContacted[address]
-                        AddressItem(
-                            address, null, contacted
-                        )
-                    }
-
-                    val bluetoothAddressItems = discoveredBluetoothAddresses.map { address ->
-                        AddressItem(
-                            address, null, null
-                        )
-                    }
-
-                    val items = peerItems + bluetoothAddressItems + addressItems
-
-                    adapter.updateItems(items)
-                    binding.txtCommunityName.text = demoCommunity.javaClass.simpleName
-                    binding.txtPeerCount.text = "${peers.size} peers"
-                    val textColorResId = if (peers.isNotEmpty()) R.color.green else R.color.red
-                    val textColor = ResourcesCompat.getColor(resources, textColorResId, null)
-                    binding.txtPeerCount.setTextColor(textColor)
-                    binding.imgEmpty.isVisible = items.isEmpty()
-
+                while (true) {
+                    loadNetworkInfo()
                     delay(1000)
                 }
             }
         }
+    }
+
+    private fun loadNetworkInfo() {
+        val demoCommunity = IPv8Android.getInstance().getOverlay<DemoCommunity>()!!
+        val peers = demoCommunity.getPeers()
+
+        val discoveredAddresses = demoCommunity.network.getWalkableAddresses(demoCommunity.serviceId)
+
+        val discoveredBluetoothAddresses =
+            demoCommunity.network.getNewBluetoothPeerCandidates().map { it.address }
+
+        val peerItems = peers.map {
+            PeerItem(
+                it
+            )
+        }
+
+        val addressItems = discoveredAddresses.map { address ->
+            val contacted = demoCommunity.discoveredAddressesContacted[address]
+            AddressItem(
+                address, null, contacted
+            )
+        }
+
+        val bluetoothAddressItems = discoveredBluetoothAddresses.map { address ->
+            AddressItem(
+                address, null, null
+            )
+        }
+
+        val items = peerItems + bluetoothAddressItems + addressItems
+
+        adapter.updateItems(items)
+        binding.txtCommunityName.text = demoCommunity.javaClass.simpleName
+        binding.txtPeerCount.text = "${peers.size} peers"
+        val textColorResId = if (peers.isNotEmpty()) R.color.green else R.color.red
+        val textColor = ResourcesCompat.getColor(resources, textColorResId, null)
+        binding.txtPeerCount.setTextColor(textColor)
+        binding.imgEmpty.isVisible = items.isEmpty()
+
+
     }
 }
